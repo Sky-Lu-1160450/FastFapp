73c230c276a68cb788345c347f8ae51b40357929
Sky-Lu-1160450 <sky.lu@lincolnuni.ac.nz>
2024-08-31 17:19:38 +1200

    update the searching function with nicce transition and history search and so on , and send request automatically

--- FFAPP/src/App.vue
+++ FFAPP/src/App.vue
@@ -20,6 +20,10 @@
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing:grayscale;
+  color: #2c3e50;
+  font-size: 14px;
+  
+
 
 }
 
--- FFAPP/src/api/search.ts
+++ FFAPP/src/api/search.ts
@@ -1,9 +1,10 @@
+import type { ISearchResultList } from '@/types'
 import axios from './base'
 
 
 
 export function fetchSearchData(key = '') {
-    return axios.get('home_search',{
+    return axios.get<ISearchResultList, ISearchResultList>('home_search',{
         params: {_label_like: key }
     })
 }
