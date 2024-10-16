ca0da58279e41741c2b283ac2382564a0b0c3084
Sky-Lu-1160450 <sky.lu@lincolnuni.ac.nz>
2024-10-17 10:16:11 +1300

    commant the developing bar

--- FFAPP/src/views/tabs/home/config.ts
+++ FFAPP/src/views/tabs/home/config.ts
@@ -4,44 +4,44 @@ import ShopList from './components/ShopList.vue'
 
 export const HOME_TABS = [
     {
-      title: 'Recommand',
+      title: 'Chef’s Recommand Shop Choice: Don’t Question It',
       value: 'recommend',
       component: ShopList,
     },
-    {
-      title: 'Sweet',
-      value: '1',
-      component: Todo,
-    },
-    {
-      title: 'Spice',
-      value: '2',
-      component: Todo,
-    },
-    {
-      title: 'Salty',
-      value: '3',
-      component: Todo,
-    },
-    {
-      title: 'Noodles',
-      value: '4',
-      component: Todo,
-    },
-    {
-      title: 'Rice',
-      value: '5',
-      component: Todo,
-    },
-    {
-      title: 'Fruit',
-      value: '6',
-      component: Todo,
-    },
-    {
-      title: 'Discount',
-      value: '6',
-      component: Todo,
-    },
+    // {
+    //   title: 'Sweet',
+    //   value: '1',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Spice',
+    //   value: '2',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Salty',
+    //   value: '3',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Noodles',
+    //   value: '4',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Rice',
+    //   value: '5',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Fruit',
+    //   value: '6',
+    //   component: Todo,
+    // },
+    // {
+    //   title: 'Discount',
+    //   value: '6',
+    //   component: Todo,
+    // },
   ]
   
